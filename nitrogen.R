1c015f5ada125e59b71613c5a764d102c1d7ece7
sally matson <sally@rstudio.us-east1-b.c.aim-covidxclimatechange.internal>
2020-07-20 20:06:15 +0000

    updated files

--- nitrogen.R
+++ nitrogen.R
@@ -1,30 +1,11 @@
 library(ncdf4)
 library(maps)
 
-filename = "OMI-Aura_L3-OMNO2d_2020m0301_v003-2020m0611t080447.he5"
-ncin <- nc_open(filename)
-
-v1 <- ncvar_get(ncin,"ColumnAmountNO2CloudScreened")
-
-v1 <- ncvar_get(ncin, "Data Fields/ColumnAmountNO2")
-dim(v1)
-
-lon <- ncvar_get(ncin, "Data Fields/phony_dim_0")
-
-heatmap(v1)
-
-plot(v1[,1])
-
-heatmap(v1, na.rm=TRUE)
-
-
-
 
 filenames <- list.files(".", pattern="*.he5")
-filenames <- filenames[seq(1,length(filenames), by=5)]
+#filenames <- filenames[seq(1,length(filenames), by=100)]
 
-setwd("~/Projects/AIM/nitrogen/data")
-out <- matrix(ncol=4, nrow=2000)
+out <- matrix(ncol=4, nrow=2009)
 row = 1
 for (file in filenames){
   filename = file
@@ -42,13 +23,8 @@ for (file in filenames){
 }
 
 
-out<- out[1:400,]
-
 plot(out[,4], type='l')
 plot(out[,2], out[,4], type='l')
-
-file = "OMI-Aura_L3-OMNO2d_2015m0101_v003-2019m1122t175558.he5"
-ncin <- nc_open(file)
 
 # 1440: longitudinal direction
 # 720: latitude
